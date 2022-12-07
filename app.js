33713709f8b85978e3bdbf22d04aaf12548ad15b
morpole <morpole@gmail.com>
2022-12-07 17:58:36 +0000

    added initial html

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // ****** SELECT ITEMS **********
-
+const alert = document.querySelector('.alert');
+const form = document.querySelector('.grocery-form');
+const grocery = document.getElementById('.grocery');
 // edit option
 
 // ****** EVENT LISTENERS **********
